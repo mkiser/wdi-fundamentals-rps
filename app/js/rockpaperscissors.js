923efc6edf7d511250b0d2e82ef752003ce4032d
Matt Kiser <matt.kiser@gmail.com>
2015-03-09 20:32:42 -0700

    bunch of changes some more

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -3,10 +3,13 @@
 ////////////////////////////////////////////////
 'use strict';
 
+
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
+    console.log("Please choose either \"rock\", \"paper\", or \"scissors\".");
     return prompt();
 }
+
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -34,6 +37,7 @@ function getComputerMove(move) {
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return move || randomPlay();
 }
+
 
 function getWinner(playerMove,computerMove) {
     var winner;
@@ -71,6 +75,8 @@ if (playerMove == "rock" || playerMove == "scissors" || playerMove == "paper") {
 return winner;
 }
 }
+
+playToFive();
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
@@ -78,15 +84,24 @@ function playToFive() {
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
     while (playerWins <5 && computerWins < 5) {
- 
+        var move;
+
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove,computerMove);
         if (winner == "player") {
             playerWins += 1;
         } 
         else if (winner == "computer") {
             computerWins +=1;
         }
+console.log("Player chose " + playerMove + ". Computer chose " + computerMove + ".");
+console.log();
+console.log("The score is " + playerWins + " to " + computerWins +".");
+
     }
 
     return [playerWins, computerWins];
 }
+
 
